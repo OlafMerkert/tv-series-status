2cfc09e10a136e50c264a0a08a58abd045860ecd
Olaf Merkert <olaf@m-merkert.de>
2013-12-19 19:13:47 +0100

    add additional dependencies to build script

--- build-core.sh
+++ build-core.sh
@@ -3,7 +3,7 @@
 if [ $1 = general ]; then
     echo "General core"
     sbcl <<EOF
-(ql:quickload '(drakma cxml local-time hunchentoot cl-who mcclim))
+(ql:quickload '(iterate cl-prevalence closure-html cl-csv split-sequence drakma cxml local-time hunchentoot cl-who mcclim))
 (sb-ext:save-lisp-and-die "general.core")
 EOF
 elif [ $1 = tvs ]; then
