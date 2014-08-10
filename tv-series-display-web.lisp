aa591819b8281d77d1edab61ade771f36fc7254b
Olaf Merkert <olaf@m-merkert.de>
2014-08-10 12:10:29 +0200

    finish porting layout to bootstrap
    + convert the table
    + style user message
    + automatically hide user help after 5 views
    + produce a button styled form filtering

--- tv-series-display-web.lisp
+++ tv-series-display-web.lisp
@@ -22,8 +22,6 @@
   (start-server)
   (run-program "/usr/bin/xdg-open" "http://localhost:8080/tv-series"))
 
-(defparameter *even-row* nil)
-
 (defmacro time-range-symbol-helper ()
   `(cond ,@(mapcar #`((string-equal time-range ,(mkstr (first a1))) ,(first a1))
                    date-filter-names)
@@ -40,54 +38,62 @@
          (episodes (filter-epi-array time-range-symbol series-symbol
                                      season-nr tse-data) ))
     (html/document (:title #1="TV Serien Status Monitor"
-                           :style "/~olaf/style.css"
-                           :library :jquery
-                           :library :bootstrap)
+                      :style "/~olaf/style.css"
+                      :library :jquery
+                      :library :bootstrap)
       ;; todo move adjustments to bootstrap to web-utils
       (:script :type "text/javascript"
-               (str (ps:ps
-                      (defun select-show (identifier)
-                        (setf (ps:@ document forms 0 series value) identifier)
-                        (@@ documents forms 0 (submit)))
-                      (defun select-season (nr)
-                        (setf (ps:@ document forms 0 season value) nr)
-                        (@@ document forms 0 (submit))))))
-      (bootstrap:navbar+
-       #1# nil
-                       )
+         (str (ps:ps
+                (defun select-show (identifier)
+                  (setf (@ document forms 0 series value) identifier)
+                  (@@ document forms 0 (submit)))
+                (defun select-season (nr)
+                  (setf (@ document forms 0 season value) nr)
+                  (@@ document forms 0 (submit)))
+                ;; after having seen the user-help messages 5 times,
+                ;; hide them
+                (symbol-macrolet ((cookie (@ document cookie)))
+                  (bind-event document ready ()
+                    (let ((current-view-count (if cookie
+                                                  (parse-int cookie)
+                                                  0)))
+                      (incf current-view-count)
+                      (setf cookie (@@ current-view-count (to-string)))
+                      (when (> current-view-count 5)
+                        (@@ ($ ".user-help") (hide)))))))))
+      (bootstrap:navbar #1#)
       (bs-body
         (:h1 #1#)
-        (:div :class "container"
-              (range-select-form series-symbol season-nr time-range-symbol))
-        (:p :class "last-update"
-            "Last update: " (str (aif (tvs-find:last-download-time)
-                                      (ol-date-utils:print-date-and-time it)
-                                      "none")))
+        (:p :class "text-primary text-right"
+           "Last update: " (str (aif (tvs-find:last-download-time)
+                                     (ol-date-utils:print-date-and-time it)
+                                     "none")))
+        (range-select-form series-symbol season-nr time-range-symbol)
         (:h2 "Liste der Episoden")
         (if (length=0 episodes)
-            (htm (:p :class "notfound" "Keine Episoden gefunden .."))
+            (htm (:p :class "bg-warning user-info text-warning" "Keine Episoden gefunden .."))
             (htm
-             (:p :class "help"
-                 "Klicke auf einen Seriennamen, um nach der Serie zu
+             (:p :class "bg-info text-info user-help"
+                "Klicke auf einen Seriennamen, um nach der Serie zu
                  filtern. Klicke auf die Titelzelle &quot;Serie&quot;,
                  um die Filterung aufzuheben.")
-             (:p :class "help"
-                 "Klicke auf eine Seasonnummer, um nach der Season zu
+             (:p :class "bg-info text-info user-help"
+                "Klicke auf eine Seasonnummer, um nach der Season zu
                  filtern. Klicke auf die Titelzelle &quot;Se&quot;, um
                  die Filterung aufzuheben.")
-             (:table :class "episodes" :style "clear: both;"
-                     (:thead
-                       (:tr :class "even"
-                            (:th :onclick (ps:ps-inline (select-show "ALLE"))
-                                 "Serie")
-                            (:th :onclick (ps:ps-inline (select-season ""))
-                                 "Se")
-                            (:th "Ep")
-                            (:th "Titel")
-                            (:th "Erstausstrahlung")))
-                     (:tbody
-                       (setf *even-row* nil)
-                       (map nil #'episode-html-row episodes)))))))))
+             (:table
+                :class "table table-striped table-condensed"
+                (:thead
+                   (:tr
+                      (:th :onclick (ps:ps-inline (select-show "ALLE"))
+                         "Serie")
+                      (:th :onclick (ps:ps-inline (select-season ""))
+                         "Se")
+                      (:th "Ep")
+                      (:th "Titel")
+                      (:th "Erstausstrahlung")))
+                (:tbody
+                   (map nil #'episode-html-row episodes)))))))))
 
 (defvar download-thread-active t
   "Set this variable to nil in order to allow downloading current
@@ -114,60 +120,62 @@
   (flet ((build-url (&key (series current-series) (season current-season) (range time-range))
            (web-utils:uri "" :series series :season season :time-range range)))
     (html/node
-      (:form :class "form-inline"
-             :method "get"
-             :action "/tv-series"
-             (:div :class "form-group"
-                   (:input :type "hidden" :name "series" :value (str current-series))
-                   (:div :class "btn-group"
-                         (:button :id "series" :type "button" :class "btn btn-default dropdown-toggle" :data-toggle "dropdown"
-                                  (esc (if (eq current-series 'alle)
-                                           "Alle"
-                                           (series-title (get-series-by-id current-series))))
-                                  (str " ")
-                                  (:span :class "caret"))
-                         (:ul :class "dropdown-menu" :role "menu"
-                              (:li (:a :href (build-url :series "ALLE") "Alle"))
-                              (dolist (series tv-series-epguides)
-                                (htm (:li (:a :href (build-url :series (identifier series))
-                                              (esc (series-title series))))))))
-                   (:div :class "input-group"
-                         (:label :class "input-group-addon" :for "season" "Season:")
-                         (:input :type "text" :name "season"
-                                 :class "form-control"
-                                 :size 2
-                                 :value (if (zerop current-season) "" (mkstr current-season))))
-                   (:input :type "hidden" :name "time-range" :value (str time-range))
-                   (:div :class "btn-group"
-                         (dolist (range-spec tvs-filter:date-filter-names)
-                           (htm (:button :type "button"
-                                         :class (if (eq time-range (first range-spec))
-                                                    "btn btn-default active"
-                                                    "btn btn-default")
-                                         :onclick (ps:ps-inline* `(setf (ps:@ location href)
-                                                                        ,(build-url :range (first range-spec))))
-                                         (str (second range-spec))))))))
+      (:form :class "inline-form"
+         :method "get"
+         :action "/tv-series"
+         (:div :class "form-group"
+            (:input :type "hidden"  :name "series" :value current-series)
+            (:div :class "btn-group"
+               (:button :type "button" :class "btn btn-default dropdown-toggle" :data-toggle "dropdown"
+                  (esc (if (eq current-series 'alle)
+                           "Alle"
+                           (series-title (get-series-by-id current-series))))
+                  (str " ")
+                  (:span :class "caret"))
+               (:ul :class "dropdown-menu" :role "menu"
+                  (:li (:a :href (build-url :series "ALLE") "Alle"))
+                  (dolist (series tv-series-epguides)
+                    (htm (:li (:a :href (build-url :series (identifier series))
+                                 (esc (series-title series)))))))))
+         (:div :class "form-group"
+            (:div :class "input-group"
+               (:label :class "input-group-addon" :for "season" "Season:")
+               (:input :type "number" :name "season"
+                  :class "form-control"
+                  :style "width: 4em"
+                  :size 2
+                  :value (if (zerop current-season) "" (mkstr current-season)))))
+         (:div :class "form-group"
+            (:input :type "hidden" :name "time-range" :value (str time-range))
+            (:div :class "btn-group"
+               (dolist (range-spec tvs-filter:date-filter-names)
+                 (htm (:button :type "button"
+                         :class (if (eq time-range (first range-spec))
+                                    "btn btn-default active"
+                                    "btn btn-default")
+                         :onclick (ps:ps-inline* `(setf (ps:@ location href)
+                                                        ,(build-url :range (first range-spec))))
+                         (str (second range-spec))))))))
       (unless download-thread-active
         (htm (:form
-               :method "get"
-               :action "/tv-series/download"
-               (:input :type "submit" :value "Herunterladen")))))))
+                :method "get"
+                :action "/tv-series/download"
+                (:input :type "submit" :value "Herunterladen")))))))
 
 (defun episode-html-row (episode)
   (html/node
-    (:tr :class (if *even-row* "even" "odd")
-         (:td :class "title"
-              :onclick (ps:ps-inline* `(select-show ,(mkstr (identifier episode))))
+    (:tr
+       (:td :class "title"
+          :onclick (ps:ps-inline* `(select-show ,(mkstr (identifier episode))))
           (esc (series-title episode)))
-         (:td :class "number"
-              :onclick (ps:ps-inline* `(select-season ,(mkstr (season-nr episode)))) 
+       (:td :class "number"
+          :onclick (ps:ps-inline* `(select-season ,(mkstr (season-nr episode))))
           (fmt "~D" (season-nr episode)))
-     (:td :class "number"
+       (:td :class "number"
           (fmt "~2,'0D" (episode-nr episode)))
-     (:td :class "title"
+       (:td :class "title"
           (esc (episode-title episode)))
-     (:td :class "date"
-          (esc (date->string (air-date episode))))
-     (notf *even-row*))))
+       (:td :class "date"
+          (esc (date->string (air-date episode)))))))
 
 ;;; todo export the list of episodes to org-mode (with dates)
